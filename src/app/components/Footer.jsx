b72c00d4be670fccb7b48534524cbbc4fa6a93da
NotVndtta <thenotalexey192@gmail.com>
2024-02-21 21:50:16 +0300

    Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-h-[81px] pl-[85px] font-normal text-[#696969] py-[27px] text-base'>
+        © 2022 JetRuby. All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
