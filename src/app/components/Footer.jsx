1f05c82b082cfa2ee98ac70ff345db41188907bd
NotVndtta <thenotalexey192@gmail.com>
2024-02-29 22:19:48 +0300

    Footer phone

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section>
 
-    <div className='max-h-[81px] pl-[85px] font-normal text-[#696969] py-[27px] text-base '>
+    <div className='ph:h-16 ph:pl-[15px] ph:text-sm ph:py-[21px] dp:h-[81px] dp:pl-[85px] font-normal text-[#696969] dp:py-[27px] dp:text-base '>
         © 2022 JetRuby. All Rights Reserved
     </div>
     </section>
--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -73,7 +73,8 @@ const Form = () => {
                 objectFit="contain" 
                 className="absolute ml-5 ph:mt-[13px] dp:mt-[23px]"
                 />
-              <input className={`ph:w-[258px] ph:h-[50px] ph:text-xs dp:w-[324px] dp:h-[55px] pl-[54px] dp:mt-2 dp:py-[13px] 
+              <input className={`ph:w-[258px] ph:h-[50px] ph:text-xs dp:w-[324px] dp:h-[55px] pl-[54px] 
+                dp:mt-2 dp:py-[13px] 
                 border ${errors.email? 'border-[#E02424]' : 'border-[#E5E7EB]'} 
                 hover:border-[#7E3AF2] focus:border-[#7E3AF2] rounded-2xl text-base font-normal`}
                 placeholder='name@example.com'
@@ -91,16 +92,19 @@ const Form = () => {
             <div className="mt-4 ph:text-xs ph:text-left dp:text-base font-semibold text-custom-black">
             Tell me about your projecty
             <textarea className="ph:w-[258px] ph:h-[122px] ph:px-5 dp:w-[667px] dp:h-[122px] dp:pl-5 pt-[13px]  border 
-                  border-[#E5E7EB] hover:border-[#7E3AF2] focus:border-[#7E3AF2] font-normal ph:text-sm dp:text-base rounded-2xl "
+                  border-[#E5E7EB] hover:border-[#7E3AF2] focus:border-[#7E3AF2] font-normal ph:text-sm 
+                  dp:text-base rounded-2xl "
               placeholder='Add here a general description of your idea and target aim' 
             {...register("description", {
               required: "Please fill this mandatory field",
             })} />
             </div>
           </div>
-            <button className="ph:flex ph:ml-[21px] ph:mt-9 dp:ml-4 max-w-[188px] max-h-[50px] justify-between dp:mt-12 px-[25px] py-[13px]
+            <button className="ph:flex ph:ml-[21px] ph:mt-9 dp:ml-4 max-w-[188px] max-h-[50px] justify-between 
+              dp:mt-12 px-[25px] py-[13px]
               text-white text-base font-semibold flex flex-row items-center gap-x-3 
-              bg-purple-600 rounded-full hover:bg-purple-500 active:bg-purple-600 active:border-purple-200 
+              bg-purple-600 rounded-full hover:bg-purple-500 active:bg-purple-600 
+              active:border-purple-200 
               disabled:bg-gray-200 disabled:text-gray-600 disabled:border-gray-200"
               disabled={!isValid}>
               <Link href="#!">
