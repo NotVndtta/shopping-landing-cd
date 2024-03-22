ad2c1f935780c577d73aebc1f8bfbd1bf1282c3c
NotVndtta <thenotalexey192@gmail.com>
2024-03-22 03:19:26 +0300

    tablet and phone mode fixes

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,25 +6,29 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section className="ph:h-[504px] ph:pl-[15px] ph:pt-[27px] tb:h-[455px] tb:flex tb:pl-[39px] 
+    <section className="ph:h-[504px] ph:pl-[15px] ph:pt-[27px]
+    tb:h-[455px] tb:flex tb:pl-[39px] tb:justify-between tb:mt-[44px]
     dp:h-[768px] dp:w-[1280px] 
-    dp:pl-[85px] dp:pt-[17px]  tb:justify-between">
+    dp:pl-[85px] dp:pt-[17px]">
       <div className="dp:pt-[162px] ">
-        <h1 className="ph:text-2xl tb:text-6xl leading-normal 
-        font-bold whitespace-nowrap ">Hey, I’m Nick</h1>
-        <p className="ph:text-sm tb:text-xl leading-normal text-custom-gray dp:text-xl
+        <h1 className="leading-normal font-bold whitespace-nowrap 
+          ph:text-2xl tb:text-6xl">
+            Hey, I’m Nick
+        </h1>
+        <p className="ph:text-sm ph:mt-1 tb:text-xl leading-normal text-custom-gray dp:text-xl
         font-normal mt-[18px] ">Frontend developer</p>
-        <div className="ph:text-sm mt-[38px] relative dp:mt-[95px] tb:text-xl  text-custom-gray 
-        font-normal  pt-6  tb:w-[445px]">
+        <div className="font-normal leading-normal mt-[38px] relative pt-6 
+          ph:text-sm ph:mt-[46px] ph:w-[290px] ph:h-[87px] ph:pr-5
+          dp:mt-[95px] tb:text-xl tb:w-[445px] 
+          text-custom-gray">
           <div className="absolute top-[-6px] left-0 w-[25px] h-[3px] bg-purple-600 "></div>
           Help you to create high-quality digital <br/> 
-          products that your clients will love and let   
-          your business thrive
+          products that your clients will love 
+          and let your business thrive
         </div>
-
-       
         <button className="ph:max-w-[139px] ph:h-[39px] ph:whitespace-nowrap ph:text-sm 
         px-[13px] py-[9px] ph:gap-x-2 ph:mt-[27px] 
+        tb:mt-[44px]
         dp:max-w-[188px] dp:h-[50px] justify-between dp:mt-[60px] dp:px-[25px] dp:py-[13px]
          text-white dp:text-base font-semibold flex flex-row items-center dp:gap-x-3 
          bg-purple-600 rounded-full hover:bg-purple-500 active:bg-purple-600 
@@ -35,8 +39,8 @@ const Hero = () => {
       </Link>
       <Image src={vector} alt="plane img"  fit="cover" />
     </button>
-      </div>
-      <div className='  bg-right bg-no-repeat dp:ml-[-180px] dp:mb-[-100px]'>
+    </div>
+      <div className='bg-right bg-no-repeat ph:mt-[-60px] ph:mr-[-50px] tb:ml-[-180px] tb:mb-[-100px] tb:mt-0'>
       <Image
         src={banner}
         alt="Banner background"
--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -8,12 +8,13 @@ const Profile = () => {
     return (
             <div className="container ph:mt-12 tb:px-[39px] dp:pl-[85px] tb:mt-0 dp:mt-[110px] ">
                     <div className="ph:text-xl ph:mx-[15px] ph:font-semibold tb:mx-0 
-                    dp:font-bold dp:text-32 leading-normal 
+                    tb:font-bold tb:text-32 leading-normal 
                         text-custom-black  ">About me
                     </div>
-                    <div className="ph:pt-9 tb:flex tb:justify-between tb:gap-x-[44px] tb:pt-20">
-                            <div className="ph:relative ph:flex ph:justify-center ph:items-center tb:w-[290px]  dp:mt-9 
-                            dp:flex dp:relative dp:w-[412px]" >
+                    <div className="ph:pt-9 ph:mx-[15px] tb:flex tb:justify-between dp:gap-x-[44px] tb:pt-20">
+                            <div className="ph:relative ph:flex ph:justify-center ph:items-center 
+                            tb:w-[290px]  
+                            dp:mt-9 dp:flex dp:relative dp:w-[412px]" >
                                 <Image src={elipsebg} alt="elipsebg" 
                                  
                                 className="z-[-1] absolute ph:w-[290px] ph:h-[290px] dp:w-[412px] dp:h-[412px] " />
@@ -21,10 +22,10 @@ const Profile = () => {
                                 className="ph:w-64 ph:h-64 dp:w-[364px] dp:h-[364px]" />
                             </div>
                         
-                        <div className="text-left relative dp:mt-[125px] tb:mt-[26px]">
+                        <div className="tb:mr-[-44px] text-left relative dp:mt-[125px] tb:mt-[26px]">
                             <div className="absolute top-[-24px] left-0 w-[25px] h-[3px] bg-purple-600"></div>
                             <div className='ph:text-sm ph:mt-12 text-custom-gray font-normal 
-                            tb:text-xl leading-normal dp:mt-0
+                            tb:text-xl leading-normal tb:mt-0
                             tb:w-[380px]  dp:w-[537px] dp:mr-[50px]'>
                                 <span className="font-semibold text-custom-black ">
                                 Nick Richardson</span> - specialist in Frontend 
@@ -34,7 +35,8 @@ const Profile = () => {
                         <button className="ph:hidden dp:flex max-w-[188px] max-h-[50px] justify-between 
                             mt-16 px-[25px] py-[13px]
                             text-white text-base font-semibold flex flex-row items-center gap-x-3
-                            bg-purple-600 rounded-full hover:bg-purple-500 active:bg-purple-600 active:border-purple-200
+                            bg-purple-600 rounded-full hover:bg-purple-500 active:bg-purple-600
+                            active:border-purple-200
                              disabled:bg-gray-200 disabled:text-gray-600 disabled:border-gray-200">
                             <Link href="#!">
                                 Get in touch
